56fcd01839f6ad35a4fbd341a6ca55894233c78b
CodyAdam <cody.ad4@gmail.com>
2023-01-29 14:14:24 +0100

    feat: add player grounded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float force = 0.3f;
+    public bool isGrounded = false;
+    public BoxCollider playerFeetCollider;
     Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
     public void onJump()
     {
-        rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
