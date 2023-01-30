6e8df94752a9e023db91c30ba15ed4747085929e
CodyAdam <cody.ad4@gmail.com>
2023-01-30 23:35:25 +0100

    feat: super fov + scene

--- Assets/Scripts/FovModifier.cs
+++ Assets/Scripts/FovModifier.cs
@@ -7,9 +7,12 @@ public class FovModifier : MonoBehaviour
     public float minFov = 15f;
     public float maxFov = 120f;
 
-    public float speedToMaxFov = 10f;
+    public float minCamDist = 10f;
+    public float maxCamDist = 4f;
+    public float speedToMax = 10f;
 
     public Cinemachine.CinemachineVirtualCamera cam;
+    public GameObject camPos;
     private Rigidbody rb;
 
     public float lerpSpeed = 0.1f;
@@ -31,9 +34,11 @@ public class FovModifier : MonoBehaviour
                 speed += current.GetComponent<Rigidbody>().velocity.magnitude;
             }
         }
-        float fov = Mathf.Lerp(minFov, maxFov, speed / speedToMaxFov);
+        float fov = Mathf.Lerp(minFov, maxFov, speed / speedToMax);
+        float dist = Mathf.Lerp(minCamDist, maxCamDist, speed / speedToMax);
 
         cam.m_Lens.FieldOfView = Mathf.Lerp(cam.m_Lens.FieldOfView, fov, lerpSpeed);
+        camPos.transform.localPosition = Vector3.Lerp(camPos.transform.localPosition, new Vector3(0, 0, dist), lerpSpeed);
     }
 
 }
